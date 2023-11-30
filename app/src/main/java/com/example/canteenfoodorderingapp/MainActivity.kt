502ade2bf2e5720964ec3910692dbff609dd4419
safitrisa <saafiitrii09@gmail.com>
2023-11-30 09:49:06 +0700

    Menambahkan Navbar

--- app/src/main/java/com/example/canteenfoodorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/canteenfoodorderingapp/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.canteenfoodorderingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.canteenfoodorderingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+        binding.navbarUmum.setOnItemSelectedListener {
+            when(it){
+                R.id.home -> replaceFragment(Home())
+                R.id.history -> replaceFragment(History())
+                R.id.status -> replaceFragment(StatusPembelian())
+                R.id.profile -> replaceFragment(Profile())
+                else -> {
+
+                }
+            }
+            true
+        }
     }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransient = fragmentManager.beginTransaction()
+        fragmentTransient.replace(R.id.framelayout,fragment)
+        fragmentTransient.commit()
+    }
+
 }
