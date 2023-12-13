ce7366539feed7189f4e69185ca3e86268079cc5
akhiko <ivankurniawan474@gmail.com>
2023-12-13 22:19:57 +0700

    testing join database

--- app/src/main/java/com/example/canteenfoodorderingapp/Profile.kt
+++ app/src/main/java/com/example/canteenfoodorderingapp/Profile.kt
@@ -1,16 +1,69 @@
 package com.example.canteenfoodorderingapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
-class Profile: Fragment() {
+
+class Profile : Fragment() {
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val user = Firebase.auth.currentUser
+        var uid: String? = null
+        var name: String? = null
+        var email: String? = null
+
+        // Get the user's UID
+        user?.let {
+            uid = it.uid
+            name = it.displayName // nama belum ditambahkan, perlu menambah form input SignIn
+            email = it.email
+        }
+
+        val root = inflater.inflate(R.layout.fragment_profile, container, false)
+        val namaUser = root.findViewById<TextView>(R.id.nama_user)
+        val emailUser = root.findViewById<TextView>(R.id.email_user)
+        val signOut: Button = root.findViewById(R.id.signout)
+        signOut.setOnClickListener {
+            firebaseAuth.signOut()
+        }
+
+        // Retrieve and display user data from database
+        if (uid != null) {
+            val databaseReference = FirebaseDatabase.getInstance().getReference("dataUsers").child(
+                uid!!
+            )
+            databaseReference.get().addOnSuccessListener { snapshot ->
+                if (snapshot.exists()) {
+                    val userData = snapshot.value as HashMap<String, Any>
+                    val nama = userData["nama"].toString()
+                    namaUser.text = "Name : $nama"
+                } else {
+                    namaUser.text = "Name : Not found"
+                }
+            }.addOnFailureListener {
+                Log.e("ProfileFragment", "Error retrieving user data", it)
+            }
+        } else {
+            Log.e("ProfileFragment", "No user logged in")
+        }
+
+        // Set the email of the user
+        emailUser.text = "Email : $email"
+        return root
     }
 }
+
--- app/src/main/java/com/example/canteenfoodorderingapp/SignUpActivity.kt
+++ app/src/main/java/com/example/canteenfoodorderingapp/SignUpActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.canteenfoodorderingapp.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
@@ -23,15 +24,41 @@ class SignUpActivity : AppCompatActivity() {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
+            val nama = binding.namaET.text.toString()
+            val noTelp = binding.noTelpET.text.toString()
+
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if (pass == confirmPass) {
                     firebaseAuth.createUserWithEmailAndPassword(email, pass)
                         .addOnCompleteListener(this) { task ->
                             if (task.isSuccessful) {
-                                // Registration successful, navigate to SignInActivity
-                                val intent = Intent(this, SignInActivity::class.java)
-                                startActivity(intent)
-                                finish() // Finish the SignUpActivity to prevent going back
+                                // Registration successful, save additional information to the database
+                                val user = firebaseAuth.currentUser
+                                user?.let {
+                                    val userId = it.uid
+                                    val databaseReference =
+                                        FirebaseDatabase.getInstance().getReference("dataUsers").child(userId)
+
+                                    val userData = HashMap<String, Any>()
+                                    userData["email"] = email
+                                    userData["nama"] = nama
+                                    userData["noTelp"] = noTelp
+
+                                    databaseReference.setValue(userData)
+                                        .addOnCompleteListener { databaseTask ->
+                                            if (databaseTask.isSuccessful) {
+                                                val intent = Intent(this, SignInActivity::class.java)
+                                                startActivity(intent)
+                                                finish() // Finish the SignUpActivity to prevent going back
+                                            } else {
+                                                Toast.makeText(
+                                                    this,
+                                                    "Failed to save user data to database",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }
+                                        }
+                                }
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Toast.makeText(
@@ -48,6 +75,7 @@ class SignUpActivity : AppCompatActivity() {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
     // Jika pengguna sudah SignIn maka akan langsung ke MainActivity
     override fun onStart() {
